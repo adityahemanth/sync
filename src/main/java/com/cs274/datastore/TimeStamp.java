81e0a4d720b514098b311de1a583069c41d12328
ravikumar <ravikumar@ucsb.cs.edu>
2016-02-29 12:22:17 -0800

    json file read + timestamp

---
+++ src/main/java/com/cs274/datastore/TimeStamp.java
@@ -0,0 +1,16 @@
+package com.cs274.datastore;
+
+
+public class TimeStamp {
+	private static long time = 0;
+
+	protected TimeStamp() {}
+
+	public static long getTime()
+	{
+		time += 1;
+		return time; 
+	}
+
+
+}
