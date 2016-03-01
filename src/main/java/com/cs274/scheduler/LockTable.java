14f42b8736dad28e4dfc0e5f568e24cced9730a8
ravikumar <ravikumar@ucsb.cs.edu>
2016-03-01 12:13:15 -0800

    12:13

--- src/main/java/com/cs274/scheduler/LockTable.java
+++ src/main/java/com/cs274/scheduler/LockTable.java
@@ -4,15 +4,32 @@ import java.util.*;
 
 public class LockTable extends HashMap{
 
-	private static LockTable table = null;
+	private static LockTable table1 = null;
+	private static LockTable table2 = null;
+	private static LockTable table3 = null;
+
 	protected LockTable() {
 
 	}
 
-	public static LockTable getInstance() {
-		if(table == null)
-			table = (LockTable) new HashMap<String, String>();
+	public static LockTable getInstanceShard1() {
+		if(table1 == null)
+			table1 = (LockTable) new HashMap<String, String>();
 
-		return table;
+		return table1;
+	}
+
+	public static LockTable getInstanceShard2() {
+		if(table2 == null)
+			table2 = (LockTable) new HashMap<String, String>();
+
+		return table2;
+	}
+
+	public static LockTable getInstanceShard3() {
+		if(table3 == null)
+			table3 = (LockTable) new HashMap<String, String>();
+
+		return table3;
 	}
 }
